abead50286d36df03101fff9084f17d9e4d12681
Phil Barker <phil@pjjk.net>
2019-02-06 16:58:09 +0000

    minor tidy

--- ocxsect.py
+++ ocxsect.py
@@ -121,7 +121,6 @@ class OCXSectionTreeProcessor(Treeprocessor):
                 node.remove(child) # remove from original place in tree
                 new_ancestors[-1].append(child) # append to the latest new ancestor
                 new_ancestors.append(child)
-
                 self.section(child, new_ancestors) # recurse through nodes children
         new_ancestors.pop()
 
