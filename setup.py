e3a8d68b1d6348549806c29a20522c0f80a0e25e
Phil Barker <phil@pjjk.net>
2019-03-05 17:41:46 +0000

    black formatting

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
+
 setup(
-    name='ocxsect',
-    version='0.1',
-    py_modules=['ocxsect'],
-    install_requires=['markdown>=3.0'],
+    name="ocxsect",
+    version="0.1",
+    py_modules=["ocxsect"],
+    install_requires=["markdown>=3.0"],
     author="Phil Barker",
     author_email="phil.barker@pjjk.co.uk",
     url="https://github.com/philbarker/ocxmd",
     description="A python markdown extension that allows you to add semantic HTML5 sectioning elements into the generated html.",
-    license="Apache2"
+    license="Apache2",
 )
--- test.py
+++ test.py
@@ -1,13 +1,14 @@
 import markdown, pytest
 from ocxsect import OCXMetadata
-#test input with various potential issues
+
+# test input with various potential issues
 # - lower case and upper case tags
 # - no blank lines around section tags
 # - section tag on first line
 # - section id with space in it
 # - section id with a # in it
 # - section id with a " in it
-TESTINPUT = '''~~C "lesson1">~~
+TESTINPUT = """~~C "lesson1">~~
 ~~h~~
 # Markdown structure test
 This is in the header section of a chapter. The chapter has id #lesson1. The header has no id.
@@ -21,14 +22,43 @@ This is in the footer of the chapter
 ~~/F~~
 ~~/C~~
 This is after the chapter
-'''
-HTMLEXPECTED = '''<chapter id="lesson1"><header><h1>Markdown structure test</h1><p>This is in the header section of a chapter. The chapter has id #lesson1. The header has no id.</p></header><section id="section1"><h1>Activity 1</h1><p>This is in a regular section (id #section1) of a chapter</p></section><footer><p>This is in the footer of the chapter</p></footer></chapter><p>This is after the chapter</p>'''
-METADATAEXPECTED = {1: {'@context': ['http://schema.org', {'oer': 'http://oerschema.org/'}, {'ocx': 'https://github.com/K12OCX/k12ocx-specs/'}], '@id': '#lesson1', 'name': 'Test Lesson 1', '@type': ['oer:Lesson', 'CreativeWork'], 'learningResourceType': 'LessonPlan', 'hasPart': {'@id': '#activity1'}, 'author': {'@type': 'Person', 'name': 'Fred Blogs'}}, 2: {'@context': ['http://schema.org', {'oer': 'http://oerschema.org/'}, {'ocx': 'https://github.com/K12OCX/k12ocx-specs/'}], '@id': '#activity1', '@type': ['oer:Activity', 'CreativeWork'], 'name': 'Test Activity 1.1', 'learningResourceType': 'Activity'}}
-STRUCTUREEXPECTED = '''\n    |--chapter{'id': 'lesson1'}\n        |--header\n            |--h1\n            |--p\n        |--section{'id': 'section1'}\n            |--h1\n            |--p\n        |--footer\n            |--p\n    |--p'''
+"""
+HTMLEXPECTED = """<chapter id="lesson1"><header><h1>Markdown structure test</h1><p>This is in the header section of a chapter. The chapter has id #lesson1. The header has no id.</p></header><section id="section1"><h1>Activity 1</h1><p>This is in a regular section (id #section1) of a chapter</p></section><footer><p>This is in the footer of the chapter</p></footer></chapter><p>This is after the chapter</p>"""
+METADATAEXPECTED = {
+    1: {
+        "@context": [
+            "http://schema.org",
+            {"oer": "http://oerschema.org/"},
+            {"ocx": "https://github.com/K12OCX/k12ocx-specs/"},
+        ],
+        "@id": "#lesson1",
+        "name": "Test Lesson 1",
+        "@type": ["oer:Lesson", "CreativeWork"],
+        "learningResourceType": "LessonPlan",
+        "hasPart": {"@id": "#activity1"},
+        "author": {"@type": "Person", "name": "Fred Blogs"},
+    },
+    2: {
+        "@context": [
+            "http://schema.org",
+            {"oer": "http://oerschema.org/"},
+            {"ocx": "https://github.com/K12OCX/k12ocx-specs/"},
+        ],
+        "@id": "#activity1",
+        "@type": ["oer:Activity", "CreativeWork"],
+        "name": "Test Activity 1.1",
+        "learningResourceType": "Activity",
+    },
+}
+STRUCTUREEXPECTED = """\n    |--chapter{'id': 'lesson1'}\n        |--header\n            |--h1\n            |--p\n        |--section{'id': 'section1'}\n            |--h1\n            |--p\n        |--footer\n            |--p\n    |--p"""
 
-md = markdown.Markdown(extensions = ['ocxsect'])
+md = markdown.Markdown(extensions=["ocxsect"])
 html = md.convert(TESTINPUT)
+
+
 def test_html():
     assert html == HTMLEXPECTED
+
+
 def test_struct():
-    assert md.tree_diagram==STRUCTUREEXPECTED
+    assert md.tree_diagram == STRUCTUREEXPECTED
